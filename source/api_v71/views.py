388e1bd49cc77d4418a2e082fce1a93407908796
Alexandr <nuvorish81@mail.ru>
2020-10-21 18:33:33 +0600

    CRUD  для Product переделан на ViewSet

--- source/api_v71/views.py
+++ source/api_v71/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseNotAllowed, JsonResponse
 from django.views.decorators.csrf import ensure_csrf_cookie
 from rest_framework.response import Response
 from rest_framework import viewsets
+from rest_framework.viewsets import ViewSet
 from webapp.models import Product, Order
 from api_v71.serializers import ProductSerializer, OrderSerializer
 
@@ -13,10 +14,49 @@ def get_token_view(request, *args, **kwargs):
         return HttpResponse()
     return HttpResponseNotAllowed('Only GET request are allowed')
 
+# class ProductViewSet(viewsets.ModelViewSet):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
 
-class ProductViewSet(viewsets.ModelViewSet):
+
+class ProductViewSet(ViewSet):
     queryset = Product.objects.all()
-    serializer_class = ProductSerializer
+
+    def list(self, request):
+        products = Product.objects.all()
+        srl = ProductSerializer(products, many=True)
+        return Response(srl.data)
+
+    def create(self, request):
+        srl = ProductSerializer(data=request.data)
+        if srl.is_valid():
+            product = srl.save()
+            return Response(srl.data)
+        else:
+            return Response(srl.errors, status=400)
+
+    def retrieve(self, request, pk=None):
+        product = get_object_or_404(Product, pk=pk)
+        srl = ProductSerializer(product)
+        return Response(srl.data)
+
+    def update(self, request, pk=None):
+        product = get_object_or_404(Product, pk=pk)
+        srl = ProductSerializer(data=request.data, instance=product)
+        if srl.is_valid():
+            product = srl.save()
+            return Response(srl.data)
+        else:
+            return Response(srl.errors, status=400)
+
+    def partial_update(self, request, pk=None):
+        pass
+
+    def destroy(self, request, pk=None):
+        product = get_object_or_404(Product, pk=pk)
+        product.delete()
+        return Response({'pk': pk})
+
 
 
 class OrderViewSet(viewsets.ModelViewSet):
